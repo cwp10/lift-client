3e79494c0d090da4ec4a53e19a9c6df28d4ad41a
WonPyoung Choi <cwp10@msn.com>
2018-09-14 13:10:12 +0900

    화면 비율에 맞춰 카메라 스케일을 조정하는 CameraAdjuster 제작

---
+++ Assets/Scripts/Components/CameraAdjuster.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Component
+{
+    [ExecuteInEditMode]
+    public class CameraAdjuster : MonoBehaviour
+    {
+        public enum CameraScaleMatchMode
+        {
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+        }
+
+        public float activeZoneRange = 8f;
+
+        [SerializeField] private CameraScaleMatchMode scaleMatchMode_ = CameraScaleMatchMode.SCREEN_WIDTH;
+
+        private void Awake()
+        {
+            AdjustActiveZone();
+        }
+
+        public void AdjustActiveZone()
+        {
+            float screenAspect = Camera.main.aspect;
+
+            switch (scaleMatchMode_)
+            {
+                case CameraScaleMatchMode.SCREEN_WIDTH:
+                    Camera.main.orthographicSize = (1f / screenAspect) * activeZoneRange / 2f;
+                    break;
+                case CameraScaleMatchMode.SCREEN_HEIGHT:
+                    Camera.main.orthographicSize = activeZoneRange / 2f;
+                    break;
+            }
+        }
+    }
+}
+
