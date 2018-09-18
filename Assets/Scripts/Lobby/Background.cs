cc62dda9b246ee831b84c2108cb16a7f02fd8821
WonPyoung Choi <cwp10@msn.com>
2018-09-18 17:17:32 +0900

    Elevator 가 움직일때 하늘이 움직인다

---
+++ Assets/Scripts/Lobby/Background.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private Transform contentTrans_ = null;
+    [SerializeField] private RawImage rawImage_ = null;
+    private float _contentMaxYPos = 0.0f;
+
+    private void Start()
+    {
+        int count = contentTrans_.childCount;
+        _contentMaxYPos = contentTrans_.GetComponent<GridLayoutGroup>().cellSize.y * count;
+    }
+
+    private void Update()
+    {
+        float uvPoint = 1.0f - (contentTrans_.position.y / _contentMaxYPos);
+        rawImage_.uvRect = new Rect(0, uvPoint, 1, 1);
+    }
+}
